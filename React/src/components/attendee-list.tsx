6683aac6fb14350e91782ea217c32b05ef9b05ec
Caio Nikolas <caio.kanu@hotmail.com>
2024-08-12 22:39:59 -0300

    filter and url state

--- React/src/components/attendee-list.tsx
+++ React/src/components/attendee-list.tsx
@@ -21,41 +21,87 @@ interface Attendee {
 }
 
 export function AttendeeList(){
-  const [search, setSearch] = useState('')
-  const [page, setPage] = useState(1)
+  const [search, setSearch] = useState(() => {
+    const url = new URL(window.location.toString())
+
+    if (url.searchParams.has('search')) {
+      return url.searchParams.get('search') ?? ''
+    }
+
+    return ''
+  })
+  const [page, setPage] = useState(() => {
+    const url = new URL(window.location.toString())
+
+    if (url.searchParams.has('page')) {
+      return Number(url.searchParams.get('page'))
+    }
+
+    return 1
+  })
+
   const [total, setTotal] = useState(0)
   const [attendees, setAttendees] = useState<Attendee[]>([])
 
   const totalPages = Math.ceil(total/10)
 
 useEffect(() => {
-  fetch(`http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees?pageIndex=${page - 1}`)
+  const url = new URL('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees')
+
+  url.searchParams.set('pageIndex', String(page - 1))
+  if(search.length > 0) {
+    url.searchParams.set('query', search)
+  }
+
+  fetch(url)
   .then(response => response.json())
   .then(data => {
     console.log(data)
-    setAttendees(data.attendees)
+    setAttendees(data.attendees) 
     setTotal(data.total)
   })
-}, [page])
+}, [page, search])
+
+function setCurrentSearch(search: string) {
+  const url = new URL(window.location.toString())
+ 
+  url.searchParams.set('search', search)
+
+  window.history.pushState({}, "", url)
+
+  setSearch(search)
+}
+
+function setCurrentPage(page: number){
+  const url = new URL(window.location.toString())
+ 
+  url.searchParams.set('page', String(page))
+
+  window.history.pushState({}, "", url)
+
+  setPage(page)
+}
 
   function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>){
-    setSearch(event.target.value)
+    setCurrentSearch(event.target.value)
+    setCurrentPage(1)
   }
 
   function goToNextPage(){
-    setPage(page + 1)
+    setCurrentPage(page + 1)
+
   }
 
   function goToPreviousPage(){
-    setPage(page - 1)
+    setCurrentPage(page - 1)
   }
 
   function goToFirstPage() {
-    setPage(1)
+    setCurrentPage(1)
   }
 
   function goToLastPage() {
-    setPage(totalPages)
+    setCurrentPage(totalPages)
   }
 
   return (
@@ -64,10 +110,13 @@ useEffect(() => {
         <h1 className="text-2xl">Participantes</h1>
         <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg flex items-center gap-3">
           <Search className='size-4 text-emerald-300' />
-          <input onChange={onSearchInputChanged} className="bg-transparent flex-1 outline-none border-0 p-0 text-sm ring-0" placeholder="Buscar participante..."/>
+          <input 
+            onChange={onSearchInputChanged}
+            value={search}
+            className="bg-transparent flex-1 outline-none border-0 p-0 text-sm ring-0 focus:ring-0" 
+            placeholder="Buscar participante..."
+          />
         </div>
-
-        {search}
       </div>  
 
       <Table>
